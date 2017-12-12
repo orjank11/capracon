31dc290cb28e1c225aa4cfa8907481af0c2bac9d
orjank11 <orjank11@users.noreply.github.com>
2017-12-12 20:40:09 +0100

    Added font, added vertical timeline component

--- src/components/Defaults/Header.js
+++ src/components/Defaults/Header.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "./Defaults.scss";
-
 const Header = () => (
   <header className="Header">
     <svg
@@ -10,14 +8,20 @@ const Header = () => (
       className="cls-1"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 100 100"
+      fill="#FFFFFF"
     >
-      <title>Ü Logo</title>
+      <title>Capra Logo</title>
       <path
         xmlns="http://www.w3.org/2000/svg"
         className="cls-2"
         d="M47.79,64.35A14.47,14.47,0,1,1,61,59.57c.08-.09,15.8-6,19.21-7.23.05-.75.08-1.5.08-2.26a30.2,30.2,0,1,0-30.2,30.19c14.74,0,27-10.06,29.67-24-6.2,2.56-10.16,4-10.16,4s-2.64,1-8.49,3A28.63,28.63,0,0,1,47.79,64.35Z"
       />
     </svg>
+    <article>
+      <h1>Capracon i New York</h1>
+      <h3>12. – 13. januar</h3>
+      <p>Det blir så mye faglig påfyll — at du neppe vil se deg tilbake.</p>
+    </article>
   </header>
 );
 
