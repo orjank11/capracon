be2035bf99f5a8a56a582eab8a1dcf23a64975d5
orjank11 <orjank11@users.noreply.github.com>
2017-12-13 22:12:14 +0100

    Added program

---
+++ src/constants/friday.js
@@ -0,0 +1,97 @@
+const friday = [
+  {
+    type: "info",
+    speaker: "Komiteen",
+    time: "09:00 - 09:15",
+    header: "Oppstart: Velkommen, info",
+    article: ""
+  },
+  {
+    type: "tech",
+    speaker: "Håkon Matland",
+    time: "09:15 - 10:00",
+    header: "AWS for ikke-teknikere",
+    article: ""
+  },
+  {
+    type: "tech",
+    speaker: "Colin Coleman",
+    time: "10:00 - 10:45",
+    header: "DevOps for Tekniske prosjektledere og tech lead",
+    article: ""
+  },
+  {
+    type: "break",
+    speaker: "",
+    time: "10:45 - 11:00",
+    header: "Pause",
+    article: ""
+  },
+  {
+    type: "office",
+    speaker: "Martin Ervik og Jarl Simonsen",
+    time: "11:00 - 11:50",
+    header: "The Capra GDPR Way",
+    article: ""
+  },
+  {
+    type: "tech",
+    speaker: "Eivind Solbakken",
+    time: "12:30 - 13:15",
+    header: "Electron hos NRK",
+    article:
+      "Electron gir oss mulighet til å lage desktop-applikasjoner for Windows, Mac og Linux med bruk av kjente teknologier som HTML, CSS og JavaScript"
+  },
+  {
+    type: "tech",
+    speaker: "Henrik Steen",
+    time: "13:15 -13:45",
+    header: "Kobling mellom frontend- vs backend-applikasjoner",
+    article: ""
+  },
+  {
+    type: "break",
+    speaker: "",
+    time: "13:45 -14:00",
+    header: "Pause",
+    article: ""
+  },
+  {
+    type: "tech",
+    speaker: "Øystein Løvberg",
+    time: "14:00 -14:45",
+    header: "AI hos Gjensidige",
+    article:
+      'Automatisering og bruk av teknologi som bildegjenkjenning/"AI", Bots, Talestyring, Video etc.'
+  },
+  {
+    type: "boss",
+    speaker: "Kjetil Thorbjørnsen",
+    time: "14:45 -15:30",
+    header: "Salg forteller om hva som skjer i 2018 hos kundene våre",
+    article: ""
+  },
+  {
+    type: "boss",
+    speaker: "Aslak Ege",
+    time: "15:30 -16:15",
+    header: "Strategiske tiltak 2018",
+    article: ""
+  },
+  {
+    type: "info",
+    speaker: "Komiteen",
+    time: "16:15 -16:30",
+    header: "Avslutning",
+    article: ""
+  },
+  {
+    type: "food",
+    speaker: "",
+    time: "19:00 -",
+    header: "Middag på Catch",
+    article: ""
+  }
+];
+
+export default friday;
---
+++ src/constants/saturday.js
@@ -0,0 +1,39 @@
+const saturday = [
+  {
+    type: "info",
+    speaker: "Øystein Løvberg",
+    time: "09:00 - 09:30",
+    header: "Plan for teamaktiviteter 2018",
+    article: ""
+  },
+  {
+    type: "info",
+    speaker: "Erik Drolshammer",
+    time: "09:30 - 10:00",
+    header: "Plan for faglige aktiviteter 2018",
+    article: ""
+  },
+  {
+    type: "info",
+    speaker: "",
+    time: "10:00 - 13:00",
+    header: "iPad-Challenge",
+    article: "Fag, sosialisering og severdigheter i en herlig mix"
+  },
+  {
+    type: "food",
+    speaker: "",
+    time: "19:00 -",
+    header: "Pause",
+    article: ""
+  },
+  {
+    type: "party",
+    speaker: "",
+    time: "23:00 -",
+    header: "Nattklubb",
+    article: ""
+  }
+];
+
+export default saturday;
